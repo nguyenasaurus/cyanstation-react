ce1bb292fe4237845bdb4a8157b21ba19dfd13d9
Justine Nguyen <justinexnguyen@gmail.com>
2021-11-28 23:21:07 -0500

    Padding for layout

--- src/js/components/Vision.jsx
+++ src/js/components/Vision.jsx
@@ -21,9 +21,9 @@ function Vision() {
 	}, []);
 
 	return (
-		<div className="min-h-screen">
+		<div className="flex justify-center py-16">
 			{vision.map(data => (
-				<div className="font-black" key={data.id}>
+				<div className="w-4/6 font-black" key={data.id}>
 					<h1 className="text-highlight text-4xl">{data.title}</h1>
 					<p className="text-lg leading-5">{data.copy}</p>
 					<h2 className="text-highlight text-6xl">{data.callout}</h2>
