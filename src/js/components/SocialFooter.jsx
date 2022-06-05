2a83d3c322e9c208f5576bfb4023b956f0694584
Justine Nguyen <justine@mvpr.io>
2022-06-05 21:33:14 +0100

    feat: link icons

--- src/js/components/SocialFooter.jsx
+++ src/js/components/SocialFooter.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function SocialFooter() {
-	const FacebookLogo =
-		"https://firebasestorage.googleapis.com/v0/b/cyanstationv1.appspot.com/o/assets%2Fsocial-facebook.png?alt=media&token=1ef5fc72-6841-4fe0-9035-57094854fdcd";
+	// const FacebookLogo =
+	"https://firebasestorage.googleapis.com/v0/b/cyanstationv1.appspot.com/o/assets%2Fsocial-facebook.png?alt=media&token=1ef5fc72-6841-4fe0-9035-57094854fdcd";
 	const LinkedInLogo =
 		"https://firebasestorage.googleapis.com/v0/b/cyanstationv1.appspot.com/o/assets%2Fsocial-linkedin.png?alt=media&token=9b10243a-ccac-4af4-a1ce-727835904c82";
 	const InstagramLogo =
@@ -13,17 +13,21 @@ function SocialFooter() {
 		<footer className="absolute bottom-4 left-4">
 			<ul className="flex flex-column pl-0">
 				<li className="mt-2">
-					<Link to="/">
+					<Link
+						to="https://www.instagram.com/cyan_station/"
+						target="_blank">
 						<img src={InstagramLogo} />
 					</Link>
 				</li>
-				<li className="mt-2">
+				{/* <li className="mt-2">
 					<Link to="/">
 						<img src={FacebookLogo} />
 					</Link>
-				</li>
+				</li> */}
 				<li className="mt-2">
-					<Link to="/">
+					<Link
+						to="https://www.linkedin.com/in/car-martin-a5492a53/"
+						target="_blank">
 						<img src={LinkedInLogo} />
 					</Link>
 				</li>
